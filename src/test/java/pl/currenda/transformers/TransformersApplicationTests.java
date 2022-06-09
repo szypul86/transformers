d8f5f4d6ec839069a17e759261917960415b2fc3
Tomasz Kempiński <tomasz.kempinski@currenda.pl>
2022-06-09 10:06:13 +0200

    Add new Spring Boot project

---
+++ src/test/java/pl/currenda/transformers/TransformersApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.currenda.transformers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransformersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
